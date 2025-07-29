286d27ce1dfb2c19f1c805c71984f307b8c409d1
Aloy Aditya Sen <aloysen951@gmail.com>
2025-07-30 02:48:39 +0530

    Add default ports for noVNC and Playwright

--- docker/magentic-ui-browser-docker/start.sh
+++ docker/magentic-ui-browser-docker/start.sh
@@ -4,5 +4,9 @@ set -e
 echo "Starting services..."
 echo "DISPLAY=$DISPLAY"
 
+# Ensure default ports are set if not provided at runtime
+NO_VNC_PORT=${NO_VNC_PORT:-6080}
+PLAYWRIGHT_PORT=${PLAYWRIGHT_PORT:-37367}
+
 # Start supervisord to manage all processes
 exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
